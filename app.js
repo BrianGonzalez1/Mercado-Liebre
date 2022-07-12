0e455bd66b3f1cc4ad90c1c6d9578eefdb6e88f5
BrianGonzalez1 <brian.eg50@gmail.com>
2022-07-11 21:09:02 -0300

    subiendo segunda parte del tp

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
 const app = express();
+const path = require("path");
+const port = 3000;
 
-app.use(express.static(path.join(__dirname,'/public')))
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/home.html")))
+app.use(express.static(path.join(__dirname, 'public')))
 
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
 
-app.listen(3000, () => console.log("Servidor esta levantado!"))
+app.listen(port, () => console.log(`servidor abierto en el puerto ${port}`))
